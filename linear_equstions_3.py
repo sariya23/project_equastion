da1cbce47ec07838fa667a95e737cb119e569101
sariya <92534075+sariya23@users.noreply.github.com>
2021-12-29 18:38:38 +0300

    Equations linear hard

---
+++ linear_equstions_3.py
@@ -0,0 +1,46 @@
+from random import randint, choice
+from sympy import solveset, Eq, Symbol
+from operator import add, truediv, mul, sub
+
+# 5*(x + 3) = 3(3 - x)
+
+
+def generation_kkk():
+    """
+    Функция генерирует коэфф
+    """
+    k1 = randint(1, 100)
+    k2 = randint(1, 100)
+    k3 = randint(1, 100)
+    k4 = randint(1, 100)
+    return k1, k2, k3, k4
+
+
+def generate_chars_eq():
+    """
+    Функция генерирует две операции для уравнения
+    """
+    operation = ['+', '-']
+    char1 = choice(operation)
+    char2 = choice(operation)
+    operation_dict = {
+        '+': add,
+        '-': sub,
+    }
+    return operation_dict[char1], operation_dict[char2], char1, char2
+
+
+def solution():
+    """
+    Функция решает уравнение
+    """
+    char1, char2, user_char1, user_char2 = generate_chars_eq()
+    k1, k2, k3, k4 = generation_kkk()
+    equation_user = f'{k1}*({k2}{user_char1}x)={k3}*({k4}{user_char2}x)'
+    x = Symbol('x')
+    root = solveset(Eq(mul(k1, char1(k2, x)), mul(k3, char2(k4, x))))
+    return root, equation_user
+
+
+
+
