720b001f5b50b43af6dad270e281295efc59d6cd
sariya <dayzwaitik123@yandex.ru>
2022-02-28 19:23:43 +0300

    Добавлена новая кнопка помощи Добавлены методы help_event, help_btn Добавлена переменная flag, чтобы корректно выводить подсказки

--- GUI_TRAINING.py
+++ GUI_TRAINING.py
@@ -3,14 +3,11 @@ from PyQt5.QtWidgets import QApplication, QWidget
 from PyQt5 import uic
 
 import sys
-import os
 import quadranic as q
 import linear_equations_1 as l1
 import linear_equations_2 as l2
 import linear_equstions_3 as l3
 
-# pushButton = pro_line
-# 24.02 остановился на выравнивании уравнений все отсальное работает
 
 class App(QWidget):
     def __init__(self):
@@ -22,12 +19,14 @@ class App(QWidget):
         self.btn_check()
         self.lvl_choice()
         self.hide_dif()
+        self.help_btn()
         self.counter_try = 0
         self.answer = 0
         self.easter_egg = 0
+        self.flag = 0
 
     def start(self):  # start the app
-        self.ui = uic.loadUi('train.ui')
+        self.ui = uic.loadUi('C:/Users/HP/PycharmProjects/project/train.ui')
         self.ui.show()
 
     def ui_init_set(self):  # settings window
@@ -36,12 +35,23 @@ class App(QWidget):
         self.ui.setFixedSize(1000, 700)
         self.ui.setWindowTitle('Doceo')
         self.ui.eq_label.setAlignment(Qt.AlignCenter)
+        self.ui.help_btn.hide()
+
+    def help_btn(self):
+        self.ui.help_btn.clicked.connect(lambda: self.help_event())
+
+    def help_event(self):
+        if self.flag != 0:
+            if self.flag == 1:
+                self.ui.help_lbl.setText(f'D = {self.d}\nx1 = {self.x1}\nx2 = {self.x2}')
+            elif self.flag == 2:
+                self.ui.help_lbl.setText(f'x = {self.x}')
 
     def info_win_btn(self):  # scd window btn
         self.ui.inst.clicked.connect(lambda: self.show_info())
 
     def show_info(self):  # scd window with instruction
-        self.ui_info = uic.loadUi('instruction.ui')
+        self.ui_info = uic.loadUi('C:/Users/HP/PycharmProjects/project/instruction.ui')
         self.ui_info.setFixedSize(800, 530)
         self.ui_info.show()
 
@@ -57,45 +67,56 @@ class App(QWidget):
     def output_qua(self):  # output eq
         eq, self.d, self.x1, self.x2 = q.quadratic()
         self.ui.eq_label.setText(
-            f'{eq}\nD={self.d}\nx1={self.x1}\nx2={self.x2}')
+            f'{eq}\n')#D={self.d}\nx1={self.x1}\nx2={self.x2}')
         self.answer = str(self.x1 + self.x2)
         self.disable_btn()
-        print(self.answer)
+        #print(self.answer)
         self.ui.answer.setText('')
         self.easter_egg = 1
+        self.flag = 1
+        self.ui.help_lbl.setText('')
+        self.ui.help_btn.hide()
 
     def output_line_ez(self):  # set eq in lbl and check answer ez
         eq, self.x = l1.answer_ez()
         if self.int_answer(self.x):
             self.x = int(self.x)
-        self.ui.eq_label.setText(f'{eq}\nx={self.x}')
+        self.ui.eq_label.setText(f'{eq}\n')#x={self.x}')
         self.answer = str(self.x)
-        # self.disable_btn()
-        print(self.answer)
+        #print(self.answer)
         self.ui.answer.setText('')
         self.easter_egg = 1
+        self.flag = 2
+        self.ui.help_lbl.setText('')
+        self.ui.help_btn.hide()
 
     def output_lien_avg(self):  # set eq in lbl and check answer avg
         eq, self.x = l2.answer_avg()
-        print(self.x)
+        #print(self.x)
         if self.int_answer(self.x):
             self.x = int(self.x)
-        self.ui.eq_label.setText(f'{eq}\nx={self.x}')
+        self.ui.eq_label.setText(f'{eq}\n')#x={self.x}')
         self.answer = str(self.x)
-        # self.disable_btn()
+        self.disable_btn()
         self.ui.answer.setText('')
         self.easter_egg = 1
+        self.flag = 2
+        self.ui.help_lbl.setText('')
+        self.ui.help_btn.hide()
 
     def output_line_pro(self):  # set eq in lbl and check answer pro
         eq, self.x = l3.answer_hard()
         print(self.x)
         if self.int_answer(self.x):
             self.x = int(self.x)
-        self.ui.eq_label.setText(f'{eq}\nx={self.x}')
+        self.ui.eq_label.setText(f'{eq}\n')#x={self.x}')
         self.answer = str(self.x)
         #self.disable_btn()
         self.ui.answer.setText('')
         self.easter_egg = 1
+        self.flag = 2
+        self.ui.help_lbl.setText('')
+        self.ui.help_btn.hide()
 
     def btn_check(self):  # check answer btn
         self.ui.cheker.clicked.connect(lambda: self.check_event())
@@ -112,6 +133,9 @@ class App(QWidget):
                 self.counter_try += 1
                 self.counter_try = str(self.counter_try)
                 self.ui.counter.setText(self.counter_try)
+
+                if int(self.counter_try) >= 3:
+                    self.ui.help_btn.show()
             else:
                 self.enable_bnt()
                 self.ui.corr.setText('Молодец, все правильно')
